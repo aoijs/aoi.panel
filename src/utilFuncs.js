0aa5d83914176a15838982eea29ff18d8d83f659
Leref <12578483+Leref@users.noreply.github.com>
2024-01-24 17:40:41 -0500

    clean(junk)

--- src/utilFuncs.js
+++ src/utilFuncs.js
@@ -4,27 +4,27 @@ const fetch = require("node-fetch")
 async function checkVersion() {
 
 
-  const data = await fetch('https://registry.npmjs.com/@akarui/aoi.panel').then(r => r.json());
-  let c_version = require('../package.json').version
-  if (c_version.includes("dev")===true) return console.log("You are using dev version of akarui/aoi.panel.");
-  let l_version = data["dist-tags"].latest;
-  if (c_version !== l_version) {
-    console.log("\x1b[33m%s\x1b[0m","You are not using the latest version of @akarui/aoi.panel!\nCurrent Version:" + c_version + "\nLatest Version:" + l_version);
-  }
+    const data = await fetch('https://registry.npmjs.com/@akarui/aoi.panel').then(r => r.json());
+    let c_version = require('../package.json').version
+    if (c_version.includes("dev") === true) return console.log("You are using dev version of akarui/aoi.panel.");
+    let l_version = data["dist-tags"].latest;
+    if (c_version !== l_version) {
+        console.log("\x1b[33m%s\x1b[0m", "You are not using the latest version of @akarui/aoi.panel!\nCurrent Version:" + c_version + "\nLatest Version:" + l_version);
+    }
 
 }
 
 function checkPackage() {
 
-  let c_name = require('../package.json').name;
-  
-  if (c_name !== "@akarui/aoi.panel") {
-    console.log("\x1b[33m%s\x1b[0m","You are not using the official version of @akarui/aoi.panel! The Developers at akaruiDevelopment are not responsible for anything going wrong with your bot! Install the official panel using: npm i @akarui/aoi.panel");
-  }
+    let c_name = require('../package.json').name;
+
+    if (c_name !== "@akarui/aoi.panel") {
+        console.log("\x1b[33m%s\x1b[0m", "You are not using the official version of @akarui/aoi.panel! The Developers at akaruiDevelopment are not responsible for anything going wrong with your bot! Install the official panel using: npm i @akarui/aoi.panel");
+    }
 }
 
 
 module.exports = {
-  checkVersion,
-  checkPackage
+    checkVersion,
+    checkPackage
 }
